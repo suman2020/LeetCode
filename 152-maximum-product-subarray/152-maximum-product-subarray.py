f4101a576429ebc291dd5f42df93d92a505af7db
Suman Thapa Magar <47877229+suman2020@users.noreply.github.com>
2022-05-22 21:24:43 -0500

    Time: 93 ms (77.10%), Space: 14.3 MB (78.94%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,19 +1,39 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
-        max_so_far =  nums[0]
-        min_so_far = nums[0]
-        result = max_so_far
+#         max_so_far =  nums[0]
+#         min_so_far = nums[0]
+#         result = max_so_far
         
-        for value in nums[1:]:
-            temp_max = max(value, max_so_far*value, min_so_far * value)
-            min_so_far = min(value, max_so_far*value, min_so_far*value)
+#         for value in nums[1:]:
+#             temp_max = max(value, max_so_far*value, min_so_far * value)
+#             min_so_far = min(value, max_so_far*value, min_so_far*value)
             
-            max_so_far = temp_max
+#             max_so_far = temp_max
             
-            print(max_so_far)
-            result = max(result, max_so_far)
+#             print(max_so_far)
+#             result = max(result, max_so_far)
             
             
-        return result
+#         return result
+
+            # 2,-5 -2,-4,3
+            ans = nums[0]
+            current = 1
+            # left to right pass. Captures all potential subarrays containing first odd n negative numbers
+            for i in nums:
+                current*=i
+                ans = max(ans, current)
+                # zero is a delimiter, restart at 1. This is optimal since zero multiplied on is still zero.
+                if current == 0:
+                    current = 1
+            current = 1
+            print(ans)
+            # right to left pass capturing all potential subarrays containing last odd n negative numbers
+            for i in reversed(nums):
+                current*=i
+                ans = max(ans, current)
+                if current == 0:
+                    current = 1
+            return ans
          
