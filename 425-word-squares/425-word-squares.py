be93e51a564f61e20b07852de19631913d5fe0f5
Suman Thapa Magar <47877229+suman2020@users.noreply.github.com>
2022-05-29 17:01:55 -0500

    Time: 2497 ms (5.13%), Space: 36.4 MB (21.30%) - LeetHub

---
+++ 425-word-squares/425-word-squares.py
@@ -0,0 +1,47 @@
+class Solution:
+    def wordSquares(self, words: List[str]) -> List[List[str]]:
+        
+        # https://www.youtube.com/watch?v=xJl_PE0Wu14
+        def getPrefixes(prefix):
+            nonlocal prefixes
+            if prefix in prefixes:
+                return prefixes[prefix]
+            prefixes[prefix]=[]
+            
+            for word in words:
+                if word.startswith(prefix):
+                    prefixes[prefix].append(word)
+                
+            return prefixes[prefix]
+            
+        def backtrack(index,current):
+            nonlocal result
+            if index == N:
+                result.append(current[:])
+                return 
+            
+            
+            # debug = [word[index] for word in current]
+            # print(debug)
+            
+            prefix = ''.join([word[index] for word in current])
+            
+            for candidate in getPrefixes(prefix):
+                current.append(candidate)
+                backtrack(index+1,current)
+                current.pop()
+                
+        
+        result = []
+        N = len(words[0])
+        prefixes = {}
+    
+        for word in words:
+            backtrack(1,[word])
+            
+        return result
+                
+            
+        
+            
+        
