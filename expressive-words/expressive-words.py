81420e229c027447c937b9993e0b523a58083751
Suman Thapa Magar <47877229+suman2020@users.noreply.github.com>
2022-06-16 17:32:53 -0500

    Time: 63 ms, Memory: 13.9 MB - LeetHub

---
+++ expressive-words/expressive-words.py
@@ -0,0 +1,62 @@
+class Solution:
+    def expressiveWords(self, s: str, words: List[str]) -> int:
+        
+        ans = 0
+        
+        #https://gist.github.com/advaidya/2bd4e6c7ea01a4c4bd7167e3a6327df3
+        # https://www.youtube.com/watch?v=RYowahX_tGo
+    
+        def getLength(str_, index):
+            
+            count = 1
+            for i in range(index,len(str_)-1):
+                if str_[i]== str_[i+1]:
+                    count+=1
+                    
+                else:
+                    break
+                    
+            return count
+                
+        
+        def check(word):
+            
+            i,j=0,0 # i-> pointer for string s , j --> pointer for string word
+            
+            while i<len(s) and j < len(word):
+                
+            
+                ch_s = s[i] # current character in string s at index i
+                ch_w = word[j] # current character in string word at index j
+                
+                
+                if ch_s !=ch_w:
+                    return False
+                
+                
+                else:
+                    len1 = getLength(s,i) # get no of repeated consecutive character in string s
+                    len2 = getLength(word,j) # get no of repeatd consecutive character in string word
+                    
+                    if len1==len2:
+                        i = i+len1
+                        j = j+len2
+                        
+                        
+                    elif len2 < len1 and len1 >=3:
+                        
+                        i = i+len1
+                        j = j+len2
+                        
+                    else: # if len2 > len1 or (len2 < len1 and len1 < 3)
+                        return False
+                          
+            return i==len(s) and j==len(word)  
+            
+        
+        for word in words:
+            if check(word):
+                ans+=1
+                
+        return ans
+        
